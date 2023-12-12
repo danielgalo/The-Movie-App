2573d19e54c7004933390f984acc63ca3786b355
GMORBAS <a_gabriel.moraes.bastos@iespablopicasso.es>
2023-12-12 10:25:12 +0100

    añadidos okhttp y gson al path

--- Java/the-movie-app/src/application/TestAPI.java
+++ Java/the-movie-app/src/application/TestAPI.java
@@ -1,9 +1,28 @@
 package application;
 
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class TestAPI {
 
 	public static void main(String[] args) {
-		
+		try {
+			
+			OkHttpClient client = new OkHttpClient();
+			
+			Request request = new Request.Builder()
+					.url("https://api.themoviedb.org/3/authentication")
+					.get()
+					.addHeader("accept", "application/json")
+					.build();
+			Response response = client.newCall(request).execute();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
