eef27a4e6b42e3acf978bab40d37733c2948820b
GMORBAS <a_gabriel.moraes.bastos@iespablopicasso.es>
2023-12-14 18:01:46 +0100

    Cambiado método de obtener la respuesta de Okhttp a httpURLconnection, funciona mejor

--- Java/the-movie-app/src/application/TestAPI.java
+++ Java/the-movie-app/src/application/TestAPI.java
@@ -1,28 +1,41 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class TestAPI {
+	
+	private static final String API_KEY = "bd20833fe416765f4800bcff775ea22d";
 
 	public static void main(String[] args) {
 		try {
-			
-			OkHttpClient client = new OkHttpClient();
-			
-			Request request = new Request.Builder()
-					.url("https://api.themoviedb.org/3/authentication")
-					.get()
-					.addHeader("accept", "application/json")
-					.build();
-			Response response = client.newCall(request).execute();
-			
+      // URL de la API de TMBD para obtener información de una película
+      String url = "https://api.themoviedb.org/3/movie/550?api_key=" + API_KEY;
+
+      // Crear una conexión HTTP
+      HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+      connection.setRequestMethod("GET");
+
+      // Obtener la respuesta de la API
+      BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+      String line;
+      StringBuilder response = new StringBuilder();
+      while ((line = reader.readLine()) != null) {
+          response.append(line);
+      }
+      reader.close();
+
+      // Imprimir la respuesta
+      System.out.println(response.toString());
 		} catch (IOException e) {
-			e.printStackTrace();
-		}
+      e.printStackTrace();
+  	}
+		
+		
+		
 	}
 
 }
