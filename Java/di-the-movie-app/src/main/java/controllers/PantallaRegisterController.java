0194070e2812f950a515a7784950c1a4e6335a7a
unknown <a_daniel.galo.vega@iespablopicasso.es>
2024-01-15 08:26:49 +0100

    Arreglo errores del proyecto al hacer merge

--- Java/di-the-movie-app/src/main/java/controllers/PantallaRegisterController.java
+++ Java/di-the-movie-app/src/main/java/controllers/PantallaRegisterController.java
@@ -1,35 +1,41 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
 public class PantallaRegisterController {
-	
+
 	@FXML
 	private Pane mainPane;
-	
+
 	@FXML
 	private Pane subPane;
-	
+
 	@FXML
 	private TextField txtEmail;
-	
+
 	@FXML
 	private TextField txtContrasena;
-	
+
 	@FXML
 	private TextField txtRepetirContrasena;
-	
+
 	@FXML
 	private Label lblRegistro;
-	
+
 	@FXML
 	private Label lblEmail;
-	
+
 	@FXML
 	private Label lblContrasena;
-	
+
 	@FXML
 	private Label lblRepetirContrasena;
-	
+
 	@FXML
 	private Button btnRegistrarse;
-	
+
 }
